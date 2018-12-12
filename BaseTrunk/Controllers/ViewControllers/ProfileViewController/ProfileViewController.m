f97b8e75eddd5bd165c823d999a13755f90119f0
45189608@qq.com <45189608@qq.com>
2018-12-12 23:04:45 +0800

    fix profile

--- BaseTrunk/Controllers/ViewControllers/ProfileViewController/ProfileViewController.m
+++ BaseTrunk/Controllers/ViewControllers/ProfileViewController/ProfileViewController.m
@@ -11,6 +11,9 @@
 
 @interface ProfileViewController ()
 
+@property (nonatomic, strong) UIImageView *bgView;
+@property (nonatomic, assign) CGRect originalFrame;
+
 @end
 
 @implementation ProfileViewController
@@ -18,6 +21,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"icon_set"] style:UIBarButtonItemStylePlain target:self action:@selector(clickRightBarButtonItem)];
+    
+//    self.bgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, 160)];
+//    self.bgView.image = [UIImage imageNamed:@"i_queshengtu_336"];
+//    self.originalFrame = self.bgView.frame;
+//    [self.view addSubview:self.bgView];
+//    self.tableView.tableHeaderView = self.bgView;
 }
 
 - (void)clickRightBarButtonItem
@@ -26,6 +35,57 @@
     [self.navigationController pushViewController:settingViewController animated:YES];
 }
 
+//- (UIImage *)imageWithColor:(UIColor *)color
+//{
+//    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+//    UIGraphicsBeginImageContext(rect.size);
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+//    CGContextSetFillColorWithColor(context, [color CGColor]);
+//    CGContextFillRect(context, rect);
+//    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//
+//    return theImage;
+//}
+//
+//- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+//{
+//    CGFloat yOffset = scrollView.contentOffset.y;
+//    CGFloat offsetToShow = ScreenWidth * 280. / 375. - YCNavbar_Height;
+//    NSLog(@"%f", scrollView.contentOffset.y);
+//    if (scrollView.contentOffset.y > offsetToShow) {
+//        CGFloat alpha = (scrollView.contentOffset.y - offsetToShow) / (2*YCNavbar_Height);
+//        if (alpha >= 0 && alpha <= 1)
+//        {
+//            self.title = @"我";
+//            [self.navigationController.navigationBar setBackgroundImage:[self imageWithColor:[UIColor colorFromHexString:@"#212121" withAlpha:alpha]] forBarMetrics:UIBarMetricsDefault];
+//            self.navigationController.navigationBar.tintColor = [UIColor colorWithWhite:0 alpha:alpha];
+//            [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor colorWithWhite:0 alpha:alpha]}];
+//        }
+//    } else {
+//        self.title = @"";
+//        self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+//        [self.navigationController.navigationBar setBackgroundImage:[self imageWithColor:[UIColor colorFromHexString:@"#212121" withAlpha:0.]] forBarMetrics:UIBarMetricsDefault];
+//    }
+//
+//    // 往上滑动效果、处理放大效果
+//    if (yOffset > 0) {
+//        self.bgView.frame = ({
+//            CGRect frame = self.bgView.frame;
+////            frame.origin.y = self.originalFrame.origin.y - yOffset;
+//            frame.origin.y = 0;
+//            frame;
+//        });
+//    } else { // 往下移动，放大效果
+//        self.bgView.frame = ({
+//            CGRect frame = self.originalFrame;
+//            frame.size.height = self.originalFrame.size.height - yOffset;
+//            frame.size.width = frame.size.height/(ScreenWidth / 160.);
+//            frame.origin.x = self.originalFrame.origin.x - (frame.size.width - self.originalFrame.size.width)/2;
+//            frame;
+//        });
+//    }
+//}
 - (id)paramsObject:(BOOL)more
 {
     return nil;
